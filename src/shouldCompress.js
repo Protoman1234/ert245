fbf38451ed7c4f55b2fe9b4d502a46ce9d1a2f9d
DemonGodXY <gfxks457@proton.me>
2024-07-03 09:23:54 +0530

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -1,13 +1,18 @@
 const MIN_COMPRESS_LENGTH = 512; // Adjust the minimum compress length as desired
 const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 2;
 
-function shouldCompress(req) {
-  const { originType, originSize, webp } = req.params;
+function shouldCompress(originType, originSize, webp) {
+	if ( !originType.startsWith("image") || originSize === 0) return false;
+	if (webp && originSize < MIN_COMPRESS_LENGTH) return false;
+	if (	// if png or gif image, and size is less than compression limit (also shouldn't be webp)
+		!webp &&
+		(originType.endsWith("png") || originType.endsWith("gif")) &&
+		originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
+	) {
+		return false;
+	}
 
-  if (!originType.startsWith('image') && originSize === 0 && webp && originSize < MIN_COMPRESS_LENGTH && !webp && originType.endsWith('gif') && originSize < MIN_TRANSPARENT_COMPRESS_LENGTH) {
-    return false;
-  }
-  return true;
+	return true;
 }
 
 module.exports = shouldCompress;
