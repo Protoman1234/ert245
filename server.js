de8e6dccd4cfec726b722915815a96be0e7a76ee
DemonGodXY <gfxks457@proton.me>
2024-07-03 14:42:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 'use strict'
-//const compression = require('compression')
+
 const express = require('express')
 const app = express()
 const authenticate = require('./src/authenticate')
@@ -8,7 +8,7 @@ const params = require('./src/params')
 const proxy = require('./src/proxy')
 
 const PORT = 8080
-//app.use(compression())
+
 app.enable('trust proxy')
 app.get('/', authenticate, params, proxy)
 app.get('/favicon.ico', (req, res) => res.status(204).end())
