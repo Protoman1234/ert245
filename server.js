2c444c0c8e4e58307b13ca1f553d2776e3b2165f
DemonGodXY <gfxks457@proton.me>
2024-07-04 10:58:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ const PORT = 8080
 app.enable('trust proxy')
 app.get('/', authenticate, params, proxy)
 app.get('/favicon.ico', (req, res) => res.status(204).end())
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+app.listen(PORT);
