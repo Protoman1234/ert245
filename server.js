37f6af1ecd3f3743af95885dda56085bb375d0ca
DemonGodXY <gfxks457@proton.me>
2024-07-08 12:04:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,14 @@
 #!/usr/bin/env node
 'use strict'
-const cluster = require("cluster");
-const os = require("os");
 
-if (cluster.isPrimary) {
-  const numClusters = process.env.CLUSTERS || (os.availableParallelism ? os.availableParallelism() : (os.cpus().length || 2))
-
-  console.log(`Primary ${process.pid} is running. Will fork ${numClusters} clusters.`);
-
-  // Fork workers.
-  for (let i = 0; i < numClusters; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died. Forking another one....`);
-    cluster.fork();
-  });
-
-  return true;
-}
-
-const express = require('express')
-const app = express()
+const fastify = require("fastify")({ logger: true })
 const authenticate = require('./src/authenticate')
 const params = require('./src/params')
 const proxy = require('./src/proxy')
-
+const fastify = Fastify({ trustProxy: true })
 const PORT = process.env.PORT || 8080
 
-app.enable('trust proxy')
-app.get('/', authenticate, params, proxy)
-app.get('/favicon.ico', (req, res) => res.status(204).end())
-app.listen(PORT);
+
+fastify.get('/', authenticate, params, proxy)
+fastify.get('/favicon.ico', (req, res) => res.status(204).end())
+fastify.listen(PORT);
