b15208459a1d5e3fab34ac614ff9ec9046a66ece
DemonGodXY <gfxks457@proton.me>
2024-07-08 15:38:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,16 +22,16 @@ if (cluster.isPrimary) {
 }
 
 const compression = require('compression')
-const express = require('express')
-const app = express()
-app.use(compression())
+const fastify = require('fastify')
+const app = fastify()
+
 const authenticate = require('./src/authenticate')
 const params = require('./src/params')
 const proxy = require('./src/proxy')
 
 const PORT = process.env.PORT || 8080
+app.use(compression())
 
-app.enable('trust proxy')
 app.get('/', authenticate, params, proxy)
 app.get('/favicon.ico', (req, res) => res.status(204).end())
 app.listen(PORT, () => console.log(`Worker ${process.pid}: Listening on ${PORT}`))
