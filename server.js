a391b5fef5ec2b38c8575c5116e7082613f6be83
DemonGodXY <gfxks457@proton.me>
2024-07-12 16:09:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
     if (req.secure) {
         next();
     } else {
-        res.redirect('https://' + req.headers.host + req.url);
+        res.redirect('https://' + req.headers + req.url);
     }
 });
 app.get('/', authenticate, params, proxy);
