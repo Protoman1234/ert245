50290a3d01bc0786dcbc1509dd481f16f5c19204
DemonGodXY <gfxks457@proton.me>
2024-07-15 12:24:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = 8080;
  // level: 9,
 //  threshold: 0
 //}))
-app.disable('trust proxy');
+app.enable('trust proxy');
 app.get('/', authenticate, params, proxy);
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 app.listen(PORT, () => console.log(`Worker ${process.pid}: Listening on ${PORT}`));
