be0fa0275ecb3ef0ed86b4ca0ebb3b0f0827b745
Protoman1234 <rffrgtdgd@gmail.com>
2024-07-16 15:52:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 const authenticate = require('./src/authenticate');
 const params = require('./src/params');
 const proxy = require('./src/proxy');
+const spdy = require('spdy');
+const fs = require('fs');
 
 const PORT = 8080;
 
@@ -15,4 +17,21 @@ threshold: 0
 app.enable('trust proxy');
 app.get('/', authenticate, params, proxy);
 app.get('/favicon.ico', (req, res) => res.status(204).end());
-app.listen(PORT, () => console.log(`Worker ${process.pid}: Listening on ${PORT}`));
+
+const options = {
+
+  // Add your SSL key and certificate here
+// **optional** SPDY-specific options
+        spdy: {
+            protocols: ['h2', 'spdy/3.1', 'http/1.1'],
+            ssl: false,
+            plain: false,
+            connection: {
+                windowSize: 1024 * 1024, // Server's window size
+                // **optional** if true - server will send 3.1 frames on 3.0 *plain* spdy
+                // helpful for best performance behind SSL offload.
+                autoSpdy31: true
+            }
+}
+};
+spdy.createServer(options, app).listen(PORT, () => console.log(`Listening on ${PORT}`));
