c443fe3c3a443bb2adc0e21d0ad9e4612879a305
Protoman1234 <rffrgtdgd@gmail.com>
2024-07-16 16:44:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,20 +15,4 @@ app.enable('trust proxy');
 app.get('/', authenticate, params, proxy);
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 
-const options = {
-
-  // Add your SSL key and certificate here
-// **optional** SPDY-specific options
-        spdy: {
-            protocols: ['h2', 'spdy/3.1', 'http/1.1'],
-            ssl: false,
-            plain: false,
-            connection: {
-                windowSize: 1024 * 1024, // Server's window size
-                // **optional** if true - server will send 3.1 frames on 3.0 *plain* spdy
-                // helpful for best performance behind SSL offload.
-                autoSpdy31: true
-            }
-}
-};
 spdy.createServer(app.listen(PORT, () => console.log(`Listening on ${PORT}`));
